4af8d7bdc92bcb5b73e558a2d65c220cbea88f40
Fotso12 <tamofotso90@gmail.com>
2025-12-06 23:43:25 +0100

    feat(backend): Initialisation du projet Spring Boot et structure

---
+++ backend/collecte-epargne-backend/src/main/java/com/collecte_epargne/collecte_epargne/CollecteEpargneApplication.java
@@ -0,0 +1,13 @@
+package com.collecte_epargne.collecte_epargne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollecteEpargneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollecteEpargneApplication.class, args);
+	}
+
+}
---
+++ backend/collecte-epargne-backend/src/test/java/com/collecte_epargne/collecte_epargne/CollecteEpargneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collecte_epargne.collecte_epargne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollecteEpargneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
