aa2c9a215f2ebd2f2b665baa7e7b920d2a6b04a9
cephora52 <cephora52@gamil.com>
2025-12-27 07:03:49 +0100

    ecritures des tests pour TransactionOffline

--- backend/collecte-epargne-backend/src/main/java/com/collecte_epargne/collecte_epargne/entities/Client.java
+++ backend/collecte-epargne-backend/src/main/java/com/collecte_epargne/collecte_epargne/entities/Client.java
@@ -77,6 +77,10 @@ public class Client {
     @OneToMany(mappedBy = "client")
     private Set<Compte> comptes;
 
+    public Client() {
+
+    }
+
     public String getCodeClient() {
         return codeClient;
     }
